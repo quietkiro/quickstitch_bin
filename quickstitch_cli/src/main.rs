1ff232bdf3fb9989c26104f596efc99fa8b6d872
Slope <linearfunc@proton.me>
2024-11-21 16:33:34 +0900

    Moved wrapper code together, fixed overlapping short flags

--- quickstitch_cli/src/main.rs
+++ quickstitch_cli/src/main.rs
@@ -3,7 +3,7 @@ pub mod _cli;
 use anyhow::Result;
 use clap::{value_parser, Args, Parser, ValueEnum};
 use quickstitch as qs;
-use quickstitch::{Loaded, Stitcher};
+use quickstitch::Stitcher;
 use std::path::{Path, PathBuf};
 
 #[derive(Debug, Clone, ValueEnum)]
@@ -13,11 +13,33 @@ enum ImageFormat {
     Jpg,
     Jpeg,
 }
+
+impl ImageFormat {
+    fn into(self, quality: u8) -> qs::ImageOutputFormat {
+        match self {
+            Self::Png => qs::ImageOutputFormat::Png,
+            Self::Webp => qs::ImageOutputFormat::Webp,
+            Self::Jpg => qs::ImageOutputFormat::Jpg(quality),
+            Self::Jpeg => qs::ImageOutputFormat::Jpeg(quality),
+        }
+    }
+}
+
 #[derive(Debug, Clone, ValueEnum)]
 enum Sort {
     Natural,
     Logical,
 }
+
+impl From<Sort> for qs::Sort {
+    fn from(value: Sort) -> Self {
+        match value {
+            Sort::Natural => qs::Sort::Natural,
+            Sort::Logical => qs::Sort::Logical,
+        }
+    }
+}
+
 #[derive(Debug, Clone, Args)]
 #[group(required = true, multiple = false)]
 struct Input {
@@ -55,13 +77,13 @@ struct Cli {
     ///
     /// Stitched images will aim to be as tall as this parameter,
     /// but they may be shorter if visual elements are in the way.
-    #[clap(long, short, default_value_t = 5000)]
+    #[clap(long, short('y'), default_value_t = 5000)]
     height: usize,
 
     /// The interval at which lines of pixels are scanned. For example,
     /// a value of 5 means every 5th horizontal line of pixels will be
     /// analyzed.
-    #[clap(long, short, default_value_t = 5)]
+    #[clap(long, short('i'), default_value_t = 5)]
     scan_interval: usize,
 
     /// The threshold value between 0 and 255 for determining when a line of
@@ -69,7 +91,7 @@ struct Cli {
     /// to be used as a splitpoint regardless of the line's pixels' values,
     /// while 255 would only allow the line to be used as a splitpoint if
     /// all the pixels in the line have the same value.
-    #[clap(long, short, default_value_t = 220)]
+    #[clap(long, short('t'), default_value_t = 220)]
     #[arg(value_parser(value_parser!(u8).range(0..=255)))]
     sensitivity: u8,
 
@@ -94,35 +116,19 @@ fn main() -> Result<()> {
     let cli = Cli::parse();
 
     let stitcher = Stitcher::new();
-    let loaded: Stitcher<Loaded> = match (cli.input.images, cli.input.dir) {
+    let loaded = match (cli.input.images, cli.input.dir) {
         (Some(images), None) => {
             let paths: Vec<&Path> = images.iter().map(PathBuf::as_path).collect();
             stitcher.load(&paths, None, true)?
         }
-        (None, Some(dir)) => stitcher.load_dir(
-            &dir,
-            None,
-            true,
-            match cli.sort {
-                Sort::Natural => qs::Sort::Natural,
-                Sort::Logical => qs::Sort::Logical,
-            },
-        )?,
+        (None, Some(dir)) => stitcher.load_dir(&dir, None, true, cli.sort.into())?,
         _ => unimplemented!("arg group rules ensure only one of the two is provided"),
     };
     let stitched = loaded.stitch(cli.height, cli.scan_interval, cli.sensitivity);
 
     // TODO: handle errors here someday
     std::fs::create_dir_all(&cli.output)?;
-    let _ = stitched.export(
-        &cli.output,
-        match cli.format {
-            ImageFormat::Png => qs::ImageOutputFormat::Png,
-            ImageFormat::Webp => qs::ImageOutputFormat::Webp,
-            ImageFormat::Jpg => qs::ImageOutputFormat::Jpg(cli.quality),
-            ImageFormat::Jpeg => qs::ImageOutputFormat::Jpeg(cli.quality),
-        },
-    );
+    let _ = stitched.export(&cli.output, cli.format.into(cli.quality));
 
     Ok(())
 }
